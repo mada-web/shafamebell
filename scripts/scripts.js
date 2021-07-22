7ed9bd35fa39410db25982412b74c50ecbefef65
a.rubashka <a.rubashka@godeltech.com>
2021-07-22 10:52:35 +0300

    fix icons

--- scripts/scripts.js
+++
@@ -1,5 +0,0 @@
-const scale = 'scale(1)';
-
-document.body.style.webkitTransform =  scale;    // Chrome, Opera, Safari
-document.body.style.msTransform =   scale;       // IE 9
-document.body.style.transform = scale;     // General
